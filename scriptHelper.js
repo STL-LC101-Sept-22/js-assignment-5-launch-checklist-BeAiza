797c9f81fa8efab3c8802fd32f1f34317c2c3dd5
Belmonte's pc <belmontei9595@gmail.com>
2022-12-19 18:26:17 -0600

    Fixed!

--- scriptHelper.js
+++ scriptHelper.js
@@ -28,10 +28,10 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
 
     function validateInput(testInput) {
-       let number = Number(testInput)
-       if (number === "") {
+       
+       if (testInput === "") {
         return "Empty"
-       } else if (!isNaN(number)) {
+       } else if (!isNaN(Number(testInput))) {
         return "Is a Number";
        } else {
         return "Not a Number"
@@ -41,55 +41,56 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
 
        
-    function formSubmission(document, pilot, copilot, fuelLevel, cargoLevel) {
-        let faultyItems = document.getElementById('faultyItems');
-        let pilotStatus = document.getElementById('pilotStatus');
-        let copilotStatus = document.getElementById('copilotStatus');
-        let fuelStatus = document.getElementById('fuelStatus');
-        let cargoStatus = document.getElementById('cargoStatus');
+   function formSubmission(document, pilot, copilot, fuelLevel, cargoLevel) {
+    let faultyItems = document.getElementById('faultyItems');
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    let fuelStatus = document.getElementById('fuelStatus');
+    let cargoStatus = document.getElementById('cargoStatus');
 
-      // Validate the input values
-        let pilotValidation = validateInput(pilot);
-        let copilotValidation = validateInput(copilot);
-        let fuelLevelValidation = validateInput(fuelLevel);
-        let cargoLevelValidation = validateInput(cargoLevel);
-      
-        // Update the shuttle requirements based on the validation results
-       if (pilotValidation === "Is a Number" || copilotValidation === "Is a Number" || fuelLevelValidation === "Not a Number" || cargoLevelValidation === "Not a Number"){
+    // Validate the input values
+    let pilotValidation = validateInput(pilot);
+    let copilotValidation = validateInput(copilot);
+    let fuelLevelValidation = validateInput(fuelLevel);
+    let cargoLevelValidation = validateInput(cargoLevel);
+
+    // Update the shuttle requirements based on the validation results
+    if (pilotValidation === "Is a Number" || copilotValidation === "Is a Number" || fuelLevelValidation === "Not a Number" || cargoLevelValidation === "Not a Number") {
         alert('Make sure to enter valid information for each fields')
-       }else if(pilotValidation === "Empty" || copilotValidation === "Empty" || fuelLevelValidation === "Empty" || cargoLevelValidation === "Empty"){
+    } else if (pilotValidation === "Empty" || copilotValidation === "Empty" || fuelLevelValidation === "Empty" || cargoLevelValidation === "Empty") {
         alert('All fields must have data.')
-       }else{ faultyItems.style.visibility = "visible"
-        pilotStatus.innerHTML = `${pilot} is ready for launch!` 
+    } else {
+        faultyItems.style.visibility = "visible"
+        pilotStatus.innerHTML = `${pilot} is ready for launch!`
         copilotStatus.innerHTML = `${copilot} is ready for launch!`
         let launchStatus = document.getElementById('launchStatus')
-          if (cargoLevel > 10000 && fuelLevel < 10000) {
+        if (cargoLevel > 10000 && fuelLevel < 10000) {
             launchStatus.style.color = "red"
             launchStatus.innerHTML = 'Not ready to launch!'
             fuelStatus.innerHTML = 'Fuel level is too low.'
             cargoStatus.innerHTML = 'Cargo mass is too high.'
-          } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
+        } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
             launchStatus.style.color = "red"
             launchStatus.innerHTML = 'Not ready to launch!'
             fuelStatus.innerHTML = 'Fuel level high enough for launch.'
             cargoStatus.innerHTML = 'Cargo mass is too high.'
 
-          } else if (cargoLevel <= 10000 && fuelLevel < 10000) {
+        } else if (cargoLevel <= 10000 && fuelLevel < 10000) {
             launchStatus.style.color = "red"
             launchStatus.innerHTML = 'Not ready to launch!'
             fuelStatus.innerHTML = 'Fuel level is too low.'
             cargoStatus.innerHTML = 'Cargo mass low enough for launch.'
-          } else {
+        } else {
             launchStatus.style.color = "green"
             launchStatus.innerHTML = 'Ready to launch!'
             fuelStatus.innerHTML = 'Fuel level high enough for launch.'
             cargoStatus.innerHTML = 'Cargo mass low enough for launch.'
-       }
-      
-      }
-      
+        }
 
-      }
+    }
+
+
+}
                
 
 
